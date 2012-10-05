95809d96bd073a43ddaf4092d356e86396f3683d
Ayende Rahien <ayende@ayende.com>
2012-10-05 19:07:42 +0200

    Test for events recovery

--- Rhino.Events.Tests/BasicPersistence.cs
+++ Rhino.Events.Tests/BasicPersistence.cs
@@ -1,4 +1,5 @@
-﻿using Rhino.Events.Tests.Events;
+﻿using System.IO;
+using Rhino.Events.Tests.Events;
 using Xunit;
 using System.Linq;
 
@@ -44,9 +45,41 @@ namespace Rhino.Events.Tests
 
 
 				var items = s.ReadRaw("users/1").ToArray();
-
+				Assert.Equal(2, items.Length);
 				Assert.Equal("Oren Eini", ((UserNameChanged)items[0]).NewName);
 				Assert.Equal("Oren", ((NewUserCreated)items[1]).Name);
+			}
+		}
+
+		[Fact]
+		public void CanHandleCorrupteData()
+		{
+			using (var s = NewScribe())
+			{
+				s.EnqueueEventAsync("users/1", new NewUserCreated
+				{
+					UserId = "users/1",
+					Name = "Oren"
+				}).Wait();
+
+				s.EnqueueEventAsync("users/1", new UserNameChanged
+				{
+					UserId = "users/1",
+					NewName = "Oren Eini"
+				}).Wait();
+			}
+
+			// simulate file corruption
+			using (var file = File.Open(@"TestScribe\data.events", FileMode.Open, FileAccess.ReadWrite))
+			{
+				file.SetLength(file.Length - 5);
+			}
+
+			using(var s = NewScribe())
+			{
+				var items = s.ReadRaw("users/1").ToArray();
+				Assert.Equal(1, items.Length);
+				Assert.Equal("Oren", ((NewUserCreated)items[0]).Name);
 			}
 		}
 
